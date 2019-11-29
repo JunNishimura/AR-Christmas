46e046804d57ac55c717d9939a6be57563dd3024
JunNishimura <n.junjun0303@gmail.com>
2019-11-30 00:51:27 +0900

    Add screenshot functionality (user can take a picture) #4
    //Attention
    There is a need to add "Privacy - Photo Library Additions Usage Description" to Info.plist in XCode after the build.

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ARChristmas
+{
+    public class ScreenShot : MonoBehaviour
+    {
+        [DllImport("__Internal")]
+        private static extern void SaveToAlbum(string path);
+
+        public void ScreenShotPressed() 
+        {
+            string filename = "test.png";
+            string path = Application.persistentDataPath + "/" + filename;
+
+            File.Delete(path);
+            ScreenCapture.CaptureScreenshot(filename);
+            StartCoroutine(SaveToCameraroll(path));
+        }
+
+        IEnumerator SaveToCameraroll(string path) 
+        {
+            while(true) 
+            {
+                if (File.Exists(path))
+                    break;
+                yield return null;
+            }
+            
+            SaveToAlbum(path);
+        }
+    }
+}
