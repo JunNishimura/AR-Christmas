aceec5e9318a6adbda1d2f8cc10e2a273770e76b
JunNishimura <n.junjun0303@gmail.com>
2019-12-09 22:48:20 +0900

    Add save and load system with json

---
+++ Assets/Scripts/ChristmasTree.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChristmasTree : MonoBehaviour
+{
+    public List<Vector3> decoItemPositions { get; set; }
+    public List<Color> decoItemColors { get; set; }
+    private List<Light> lightsForTree;
+
+    private void Awake() 
+    {
+        decoItemPositions = new List<Vector3>();
+        decoItemColors = new List<Color>();
+        lightsForTree = new List<Light>();
+        foreach(Transform light in this.transform.Find("Lights").transform) 
+        {
+            lightsForTree.Add(light.GetComponent<Light>());
+        }
+    }
+
+    public void SetTreeLight(bool isLightON) 
+    {
+        foreach(var light in lightsForTree)
+        {
+            light.enabled = isLightON;
+        }
+    }
+}
---
+++ Assets/Scripts/Save Load/SaveLoadManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ARChristmas
+{
+    [Serializable]
+    public class TreeSaveData
+    {
+        public List<Vector3> decoItemPositions;
+
+    }   
+
+    public class SaveLoadManager : MonoBehaviour
+    {
+        public void OnSaveTree() 
+        {
+            TreeSaveData save = CreateSaveData();
+            var json = JsonUtility.ToJson(save);
+            var path = Application.persistentDataPath + "/save.txt";
+            var writer = new StreamWriter(path, false);
+            writer.WriteLine(json);
+            writer.Flush();
+            writer.Close();
+        }
+
+        public void OnLoadTree()
+        {
+            var info = new FileInfo(Application.persistentDataPath + "/save.txt");
+            var reader = new StreamReader(info.OpenRead());
+            var json = reader.ReadToEnd();
+            var data = JsonUtility.FromJson<TreeSaveData>(json);
+            // ロードしたデータを反映
+        }
+
+        private TreeSaveData CreateSaveData() 
+        {
+            TreeSaveData save = new TreeSaveData();
+            save.decoItemPositions = ObjectPlacement.christmasTree.decoItemPositions;
+
+            return save;
+        }
+    }
+}
